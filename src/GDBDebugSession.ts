b5737ad798278e7ec8084b0807a38ef6c7e03b28
Luis Penagos <luis@penagos.co>
2020-04-26 20:33:52 -0400

    Debugger groundwork

---
+++ src/GDBDebugSession.ts
@@ -0,0 +1,7 @@
+import {
+	LoggingDebugSession
+} from 'vscode-debugadapter';
+
+export class GDBDebugSession extends LoggingDebugSession {
+
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,42 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, CancellationToken, ProviderResult } from 'vscode';
+import { GDBDebugSession } from './GDBDebugSession';
+import * as Net from 'net';
+
+class GDBConfigurationProvider implements vscode.DebugConfigurationProvider {
+	private server?: Net.Server;
+
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined,
+							  config: DebugConfiguration,
+							  token?: CancellationToken): ProviderResult<DebugConfiguration> {
+		// if launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'matlab') {
+				config.type = 'vgdb';
+				config.name = 'Launch';
+				config.request = 'launch';
+			}
+		}
+
+		if (!this.server) {
+			// start listening on a random port
+			this.server = Net.createServer(socket => {
+				const session = new GDBDebugSession();
+				session.setRunAsServer(true);
+				session.start(<NodeJS.ReadableStream>socket, socket);
+			}).listen(0);
+		}
+
+		return config;
+	}
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,6 +46,9 @@ export function activate(context: vscode.ExtensionContext) {
 			value: "a.out"
 		});
 	}));
+
+	const provider = new GDBConfigurationProvider()
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('vgdb', provider));
 }
 
 export function deactivate() {
