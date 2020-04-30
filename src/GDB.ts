89f53a8ab0dcc9570a7db0581fb88fe0c528cebb
Luis Penagos <luis@penagos.co>
2020-04-30 18:09:47 -0400

    Began writing MI parser

--- src/GDB.ts
+++ src/GDB.ts
@@ -1,13 +1,22 @@
 import { spawn, ChildProcess } from "child_process";
+import { MIParser } from "./MIParser";
 
 export class GDB {
     private pHandle: ChildProcess;
     private path: string;
     private args: any;
+    private parser: MIParser;
+
+    // Output buffering for stdout pipe
+    private ob: string;
+
+    // Track if GDB is initialized
+    private initialized: boolean;
 
     public constructor() {
         this.path = 'gdb';
-        this.args = ['--interpreter=mi2'];
+        this.args = ['--interpreter=mi2', '-q'];
+        this.parser = new MIParser(this);
     }
 
     public spawn(program: string): Promise<any> {
@@ -24,13 +33,32 @@ export class GDB {
         });
     }
 
-    // Called on any stdout produced by GDB Process
-    private stdoutHandler() {
+    public isInitialized() {
+        return this.initialized;
+    }
 
+    public setInitialized() {
+        console.log("GDB is initialized");
+        this.initialized = true;
+    }
+
+    // Called on any stdout produced by GDB Process
+    private stdoutHandler(data) {
+        let str = data.toString('utf8');
+        this.ob += str;
+
+        // We may be receiving buffered output. In such case defer parser until
+        // full output has been transmitted as denoted by \n
+        let nPos = this.ob.lastIndexOf('\n')
+        if (nPos != -1) {
+            this.ob = this.ob.substr(0, nPos);
+            this.parser.parse(this.ob);
+        }
     }
 
     // Called on any stderr produced by GDB Process
-    private stderrHandler() {
-
+    private stderrHandler(data) {
+        let str = data.toString('utf8');
+        console.log("(stderr) " + str);
     }
 }
--- src/MIParser.ts
+++ src/MIParser.ts
@@ -1,3 +1,29 @@
-export class MIParser {
+import { GDB } from "./GDB";
 
+const GDB_PROMPT = '(gdb)';
+
+export class MIParser {
+    private gdb: GDB;
+
+    public constructor(gdb: GDB) {
+        this.gdb = gdb;
+    }
+
+    public parse(str: string) {
+        console.log("(stdout) " + str);
+
+        if (this.gdb.isInitialized()) {
+
+        } else {
+            // Since we suppress the copyright and version headers on startup
+            // we expect the initialization sequence to be complete when we
+            // exactly OBTAIN (gdb)
+            if (str == GDB_PROMPT) {
+                this.gdb.setInitialized();
+            } else {
+                console.log("Failed initialization");
+                process.exit(1);
+            }
+        }
+    }
 }
