51a131b94c7a1b10eefb6ee7c931a40f0c6e268c
Luis Penagos <luis@penagos.co>
2020-05-31 11:05:20 -0400

    Fix ThreadsRequest

--- src/GDBDebugSession.ts
+++ src/GDBDebugSession.ts
@@ -175,16 +175,12 @@ export class GDBDebugSession extends LoggingDebugSession {
 
     protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
         this.log(`Threads request`);
-        if (this.GDB.isStopped()) {
             this.GDB.getThreads().then((threads: Thread[]) => {
                 response.body = {
                     threads: threads
                 };
                 this.sendResponse(response);
             });
-        } else {
-            this.sendResponse(response);
-        } 
     }
 
     protected stackTraceRequest(response: DebugProtocol.StackTraceResponse,
