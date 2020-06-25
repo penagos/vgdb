66e120611491f959cf926c75a25327b2e3425872
Luis Penagos <luis@penagos.co>
2020-06-24 20:59:17 -0400

    Begin plumbing for vGDB and MIParse unittests

---
+++ tests/suite/miparser.test.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+import * as vstest from 'vscode-test';
+
+suite('MI Parser', () => {
+    test('Out of Band Record', () => {
+        console.log("todo");
+    });
+
+    test('Result Record', () => {
+        console.log("todo");
+    });
+
+    test('Async Record', () => {
+        console.log("todo");
+    });
+
+    test('Stream Record', () => {
+        console.log("todo");
+    });
+});
---
+++ tests/suite/vgdb.test.ts
@@ -0,0 +1,8 @@
+import * as vscode from 'vscode';
+import * as vstest from 'vscode-test';
+
+suite('vGDB Test Suite', () => {
+    test('Launch', () => {
+        console.log("todo");
+    });
+});
