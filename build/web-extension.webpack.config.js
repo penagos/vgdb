4203422e0af0975bb35abe43f09fbef45462599d
Luis Penagos <luis@penagos.co>
2021-09-14 21:17:09 -0400

    webpack config cruft

--- build/web-extension.webpack.config.js
+++ build/web-extension.webpack.config.js
@@ -1,26 +1,17 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-//@ts-check
 'use strict';
-
-//@ts-check
-/** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 const path = require('path');
 
-module.exports = /** @type WebpackConfig */ {
+module.exports = {
 	context: path.dirname(__dirname),
-	mode: 'production', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-	target: 'node', // extensions run in a webworker context
+	mode: 'production',
+	target: 'node',
 	entry: {
 		extension: './src/web/extension.ts',
 	},
 	resolve: {
 		mainFields: ['module', 'main'],
-		extensions: ['.ts', '.js'], // support ts-files and js-files
+		extensions: ['.ts', '.js'],
 		alias: {
 		}
 	},
@@ -29,8 +20,6 @@ module.exports = /** @type WebpackConfig */ {
 			test: /\.ts$/,
 			exclude: /node_modules/,
 			use: [{
-				// configure TypeScript loader:
-				// * enable sources maps for end-to-end source maps
 				loader: 'ts-loader',
 				options: {
 					compilerOptions: {
@@ -42,7 +31,7 @@ module.exports = /** @type WebpackConfig */ {
 		}]
 	},
 	externals: {
-		'vscode': 'commonjs vscode', // ignored because it doesn't exist
+		'vscode': 'commonjs vscode',
 	},
 	performance: {
 		hints: false
